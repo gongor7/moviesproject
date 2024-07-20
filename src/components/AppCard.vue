968a8f6b8edae542866d3815891ac04fa30dd8c0
Cristhian Leandro Góngora Torrez <crisgongor7@gmail.com>
2024-07-20 18:16:14 -0400

    Componente card para peliculas

---
+++ src/components/AppCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card class="mx-auto my-4" max-width="344">
+    <v-img :src="poster" height="200px"></v-img>
+    <v-card-title>{{ title }}</v-card-title>
+    <v-card-subtitle>{{ year }}</v-card-subtitle>
+    <v-card-text>
+      <p><strong>imdbID:</strong> {{ imdbID }}</p>
+      <p><strong>Type:</strong> {{ Type }}</p>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'AppCard',
+  props: {
+    poster: String,
+    title: String,
+    year: String,
+    imdbID: String,
+    Type: String
+  }
+}
+</script>
+
+<style scoped>
+/* Puedes añadir estilos adicionales aquí si es necesario */
+</style>
