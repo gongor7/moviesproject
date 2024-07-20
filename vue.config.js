e2ccc7ffc43a22f97931e154b10b95623e0a1d2c
Cristhian Leandro Góngora Torrez <crisgongor7@gmail.com>
2024-07-20 18:14:11 -0400

    configuraciones para vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
+// vue.config.js
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: ['vuetify'],
   devServer: {
     port: 8083 // Configura el puerto aquí
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'vuetify$': 'vuetify/lib',
+        'vue$': 'vue/dist/vue.esm.js'
+      }
+    }
   }
-})
+});
